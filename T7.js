a6136b8462aa1d11e37ecc3b00f832f79b465944
adandoang <akhdanjzif@gmail.com>
2024-04-02 12:36:51 +0700

    first commit

---
+++ T7.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video(){
+    alert("ini ceramah Ust Yazid");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("ini audio Ramadhan");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar Silaturahmi");
+});
